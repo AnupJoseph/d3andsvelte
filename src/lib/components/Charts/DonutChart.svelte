2f49f05fae96a5e2a3720fcf0c62fe88456d73a2
Anup Joseph <anup.joseph@quantiphi.com>
2022-03-01 11:30:28 +0530

    Finished Donut chart

--- src/lib/components/Charts/DonutChart.svelte
+++ src/lib/components/Charts/DonutChart.svelte
@@ -24,15 +24,36 @@
 		.outerRadius((0.95 * height) / 2);
 
 	let pieArcs = piePath(data);
-    console.log(pieArcs)
 </script>
 
-<svg {height} {width} >
-
+<svg {height} {width}>
+	<g transform={`translate(${width / 2},${height / 2})`}>
+		{#each pieArcs as pArc}
+			<path d={arcPath(pArc)} fill={colorScale(pArc.data.type)} />
+			<text transform={`translate(${labelPath.centroid(pArc)})`}>
+				<tspan x="0" dy="0" style="font-weight: bold;">{pArc.data.type}</tspan>
+				<tspan x="0" dy="1.2em" style="font-size: 12;"
+					>{`${pArc.data.value.toFixed(1)}kg`}</tspan
+				>
+			</text>
+		{/each}
+	</g>
 </svg>
 
 <style>
 	svg {
 		border: 1px solid #ccc;
+	}
+	path {
+		stroke: whitesmoke;
+		stroke-width: 2;
+	}
+	text {
+		text-anchor: middle;
+	}
+	tspan {
+		font-family: sans-serif;
+		font-size: 12;
+		fill: #222;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,3 +4,4 @@
 <div><a href="/days/day3">Day 3</a></div>
 <div><a href="/days/day4">Day 4</a></div>
 <div><a href="/days/day5">Day 5</a></div>
+<div><a href="/days/day6">Day 6</a></div>
